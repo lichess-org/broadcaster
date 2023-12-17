15f9b6480f2dde8cf5ea86a62fddcc0bd43dc31f
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-12-16 20:12:37 -0500

    improve blank slate

--- src/components/Broadcasts.vue
+++ src/components/Broadcasts.vue
@@ -5,11 +5,12 @@ import Broadcast from './Broadcast.vue';
 import { LichessBroadcast } from '../types';
 import { useSettingsStore } from '../stores/settings';
 import { useUserStore } from '../stores/user';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const settings = useSettingsStore();
 const user = useUserStore();
 
+const isLoading = ref<boolean>(true)
 const broadcasts = ref<LichessBroadcast[]>([])
 
 async function openBrowser(path: string) {
@@ -46,11 +47,22 @@ async function getBroadcasts(callback: (value: LichessBroadcast) => void) {
   })
 }
 
-getBroadcasts((value) => {
-  broadcasts.value.push(value)
-}).then(() => {
-  console.log('done fetching broadcasts')
+const hasBroadcasts = computed<boolean>(() => {
+  return broadcasts.value.length > 0
 })
+
+function refresh() {
+  broadcasts.value = []
+  isLoading.value = true
+
+  getBroadcasts((value) => {
+    broadcasts.value.push(value)
+  }).then(() => {
+    isLoading.value = false
+  })
+}
+
+refresh()
 </script>
 
 <template>
@@ -58,17 +70,40 @@ getBroadcasts((value) => {
     <div class="min-w-0 flex-1">
       <h2 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">Your Broadcasts</h2>
     </div>
-    <div class="mt-4 flex md:ml-4 md:mt-0">
+    <div class="mt-4 flex md:ml-4 md:mt-0 space-x-1">
+      <button type="button" @click="refresh"
+        class="inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Refresh</button>
       <button type="button" @click="openMyBroadcasts"
         class="inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">View
         on Lichess</button>
       <button type="button" @click="openBrowser('/broadcast/new')"
-        class="ml-3 inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">&plus;
-        Create a new broadcast</button>
+        class="inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">&plus;
+        New Broadcast</button>
     </div>
   </div>
 
-  <ul role="list" class="divide-y divide-white/5">
+  <ul v-if="hasBroadcasts" role="list" class="divide-y divide-white/5">
     <Broadcast v-for="broadcast in broadcasts" :broadcast="broadcast" />
   </ul>
+
+  <div v-if="!hasBroadcasts && !isLoading" class="text-center mt-12">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="mx-auto h-12 w-12 text-gray-400">
+      <path stroke-linecap="round" stroke-linejoin="round"
+        d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
+    </svg>
+
+    <h3 class="mt-2 text-sm font-semibold text-gray-200">No broadcasts</h3>
+    <p class="mt-1 text-sm text-gray-300">Get started by creating a new broadcast.</p>
+    <div class="mt-6">
+      <button type="button" @click="openBrowser('/broadcast/new')"
+        class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path
+            d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+        </svg>
+        Create a new broadcast
+      </button>
+    </div>
+  </div>
 </template>
--- src/components/LichessLogin.vue
+++ src/components/LichessLogin.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import { invoke } from '@tauri-apps/api/tauri'
-
-import { useUserStore } from '../stores/user'
 import { useSettingsStore } from '../stores/settings';
 
-const user = useUserStore()
 const settings = useSettingsStore()
 
 async function loginWithLichess() {
@@ -15,20 +12,20 @@ async function loginWithLichess() {
 </script>
 
 <template>
-  <div class="text-center text-white">
-    <template v-if="user.isLoggedIn()">
-      <div>
-        Logged in as {{ user.username }}
-        -
-        <button @click="user.logout()" type="button"
-          class="inline-flex items-center justify-center rounded-md border border-transparent px-4 py-2 font-medium bg-indigo-100 text-indigo-800 hover:bg-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm">
-          Log out
-        </button>
-      </div>
-    </template>
-    <button v-else @click="loginWithLichess" type="button"
-      class="inline-flex items-center justify-center rounded-md border border-transparent px-4 py-2 font-medium bg-indigo-100 text-indigo-800 hover:bg-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm">
-      Click here to log in with Lichess
-    </button>
+  <div class="text-center mt-20">
+    <h3 class="mt-2 text-xl font-semibold text-gray-200">Welcome to the Lichess PGN Uploader app</h3>
+    <p class="mt-1 text-sm text-gray-300">Get started by logging in with your Lichess account.</p>
+    <div class="mt-6">
+      <button type="button" @click="loginWithLichess"
+        class="inline-flex items-center rounded-md bg-indigo-600 px-8 py-3 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="-ml-0.5 mr-1.5 h-5 w-5">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+        </svg>
+
+        Log in with Lichess
+      </button>
+    </div>
   </div>
 </template>
