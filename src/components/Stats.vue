103098da0d3b4a0370d1ce3e9d7bfd34b1a29143
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-12-16 19:43:03 -0500

    dashboard cleanup

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -4,36 +4,35 @@
 <template>
     <div class="bg-gray-900">
         <div class="mx-auto max-w-7xl">
-            <div class="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
+            <div class="grid grid-cols-4 gap-px bg-white/5 ">
                 <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
                     <p class="text-sm font-medium leading-6 text-gray-400">Status</p>
                     <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-4xl font-semibold tracking-tight text-white">idle</span>
+                        <span class="text-4xl font-semibold tracking-tight text-white">Idle</span>
                     </p>
                 </div>
                 <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-                    <p class="text-sm font-medium leading-6 text-gray-400">Number of rounds being synced</p>
+                    <p class="text-sm font-medium leading-6 text-gray-400">Rounds</p>
                     <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-4xl font-semibold tracking-tight text-white">4</span>
-                        <span class="text-sm text-gray-400">mins</span>
+                        <span class="text-4xl font-semibold tracking-tight text-white">0</span>
                     </p>
                 </div>
                 <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-                    <p class="text-sm font-medium leading-6 text-gray-400">Number of games sent</p>
+                    <p class="text-sm font-medium leading-6 text-gray-400">Games</p>
                     <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-4xl font-semibold tracking-tight text-white">3</span>
+                        <span class="text-4xl font-semibold tracking-tight text-white">0</span>
                     </p>
                 </div>
                 <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-                    <p class="text-sm font-medium leading-6 text-gray-400">Number of moves sent</p>
+                    <p class="text-sm font-medium leading-6 text-gray-400">Moves</p>
                     <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-4xl font-semibold tracking-tight text-white">98</span>
+                        <span class="text-4xl font-semibold tracking-tight text-white">0</span>
                     </p>
                 </div>
             </div>
         </div>
     </div>
-    <div class="bg-white mt-4 p-4">
+    <div class="bg-gray-50 mt-4 p-4 text-sm font-mono">
         [logs here]
     </div>
 </template>
