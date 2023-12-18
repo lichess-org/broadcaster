5b68a4cbf0e85957674d7cd6d1b16668cf5df269
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-12-17 20:25:18 -0500

    settings ui tweaks

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -30,38 +30,72 @@ async function openBrowser(url: string) {
 <template>
   <h2 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">Settings</h2>
 
-  <div v-if="user.isLoggedIn()" class="text-white">
-    Logged in as <strong>{{ user.username }}</strong>
+  <div class="divide-y divide-white/5">
+    <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-8 sm:px-6 md:grid-cols-3 lg:px-8">
+      <div>
+        <h2 class="text-base font-semibold leading-7 text-white">Advanced</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-400">For development purposes</p>
+      </div>
 
-    <span class="ml-2">
-      <a href="#" @click="user.logout" class="underline">Logout</a>
-    </span>
+
+      <form class="md:col-span-2" @submit.prevent="save">
+        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+          <div class="col-span-full">
+            <label for="lichessUrl" class="block text-sm font-medium leading-6 text-white">Lichess URL</label>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <input type="text" id="lichessUrl" autocomplete="false"
+                  class="flex-1 border-0 bg-transparent py-1.5 px-2 text-white focus:ring-0 sm:text-sm sm:leading-6"
+                  v-model="form.lichessUrl">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-8 flex">
+          <button type="submit"
+            class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+        </div>
+      </form>
+    </div>
+
+    <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-8 sm:px-6 md:grid-cols-3 lg:px-8"
+      v-if="user.isLoggedIn()">
+      <div>
+        <h2 class="text-base font-semibold leading-7 text-white">Session</h2>
+      </div>
+
+      <div>
+        <p class="mb-2 text-sm leading-6 text-gray-400">You are logged in as <strong>{{ user.username }}</strong></p>
+        <form class="flex items-start md:col-span-2" @submit.prevent="user.logout()">
+          <button type="submit"
+            class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400">Logout</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-8 sm:px-6 md:grid-cols-3 lg:px-8">
+      <div>
+        <h2 class="text-base font-semibold leading-7 text-white">About</h2>
+      </div>
+
+      <div class="md:col-span-2">
+        <p class="mb-2 text-sm leading-6 text-gray-400">
+          You are using version v<strong>{{ appVersion }}</strong>
+        </p>
+        <p class="mb-2 text-sm leading-6 text-gray-400">
+          See the source and contribute to this
+          <a href="#" class="underline" @click.prevent="openBrowser('https://github.com/fitztrev/pgn-broadcaster')">app on
+            GitHub</a>
+        </p>
+        <p class="mb-2 text-sm leading-6 text-gray-400">
+          If you're having trouble with your broadcast, please
+          <a href="#" class="underline"
+            @click.prevent="openBrowser('https://discord.com/channels/1094189632691904573/1122545040686858340')">
+            post to <code>#broadcast-errors</code> on the Lichess Content Discord</a>.
+        </p>
+      </div>
+    </div>
   </div>
-
-  <form class="flex flex-col space-y-4 my-8">
-    <label class="flex flex-col space-y-1">
-      <span class="text-white">Lichess URL</span>
-      <input type="text" v-model="form.lichessUrl" class="p-1" />
-      <div class="text-gray-300 text-sm">Advanced setting. Only change for development.</div>
-    </label>
-
-    <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      @click="save">Save</button>
-  </form>
-
-  <h3 class="text-white text-xl">About</h3>
-  <p class="text-white">
-    You are using version v<strong>{{ appVersion }}</strong>
-    <br />
-    See the source and contribute to this
-    <a href="#" class="underline" @click.prevent="openBrowser('https://github.com/fitztrev/pgn-broadcaster')">app on
-      GitHub</a>
-  </p>
-
-  <h3 class="text-white text-xl mt-4">Support</h3>
-  <p class="text-white">If you're having trouble with your broadcast, please
-    <a href="#" class="underline"
-      @click.prevent="openBrowser('https://discord.com/channels/1094189632691904573/1122545040686858340')">
-      post to <code>#broadcast-errors</code> on the Lichess Content Discord</a>.
-  </p>
 </template>
