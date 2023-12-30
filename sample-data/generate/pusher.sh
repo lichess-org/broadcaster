351849ac686ae4a11a338b2096481d92d3e3adda
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-12-29 22:19:26 -0500

    httpie for test script

--- sample-data/generate/pusher.sh
+++ sample-data/generate/pusher.sh
@@ -2,44 +2,53 @@
 
 LICHESS_URL="http://localhost:8080"
 API_TOKEN="lip_bobby" # with study:write permission
-BROADCAST_ROUND_ID="ayAPBpLr"
+BROADCAST_ROUND_ID="ixuNxFcc"
 
-######
+####################
 
-# Push 10 games individually
+echo $LICHESS_URL/broadcast/-/-/$BROADCAST_ROUND_ID
+
+# Push games individually
 # 1.e4 for all
-for i in {1..3}
+for i in {1..2}
 do
-    echo '
-    [White "Player A'$i'"]
-    [Black "Player B'$i'"]
-    [Result "*"]
+    echo "Pushing game $i, move 1..."
+    
+    http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< \
+'[White "Player '$i'W"]
+[Black "Player '$i'B"]
+[Result "*"]
 
-    1. e4' > output/game-$i.pgn
-
-    echo "### Pushing game $i"
-
-    time curl -X POST \
-        -H "Authorization: Bearer $API_TOKEN" \
-        -d @output/game-$i.pgn \
-        $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push
+1. e4'
 done
 
 # Update each game individually with a 2nd move
 # 1...e5 for all
-for i in {1..3}
+for i in {1..2}
 do
-    echo '
-    [White "Player A'$i'"]
-    [Black "Player B'$i'"]
-    [Result "*"]
+    echo "Pushing game $i, move 2..."
 
-    1. e4 e5' > output/game-$i.pgn
+    http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< \
+'[White "Player '$i'W"]
+[Black "Player '$i'B"]
+[Result "*"]
 
-    echo "### Pushing game $i"
-
-    time curl -X POST \
-        -H "Authorization: Bearer $API_TOKEN" \
-        -d @output/game-$i.pgn \
-        $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push
+1. e4 e5'
 done
+
+####################
+
+echo "Pushing 3rd move to all games (combined PGN)..."
+
+http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< '
+[White "Player 1W"]
+[Black "Player 1B"]
+[Result "*"]
+
+1. e4 e5 2. Nf3
+
+[White "Player 2W"]
+[Black "Player 2B"]
+[Result "*"]
+
+1. e4 e5 2. Nf3'
