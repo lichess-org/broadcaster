37c0652a7ab87f2c6b94eabd17d5f656f34e30cf
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-01-27 19:03:16 -0500

    update test script

--- sample-data/generate/pusher.sh
+++ sample-data/generate/pusher.sh
@@ -2,7 +2,7 @@
 
 LICHESS_URL="http://localhost:8080"
 API_TOKEN="lip_bobby" # with study:write permission
-BROADCAST_ROUND_ID="q11ds71x"
+BROADCAST_ROUND_ID="J1Lk2oa1"
 
 #### Part A ####
 echo "Pushing each game individually..."
@@ -19,10 +19,19 @@ http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROAD
 [Black "Black 2"]
 [Result "*"]
 
-1. e4'
+1. d4'
+
+http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< \
+'[White "White 3"]
+[Black "Black 3"]
+[Result "*"]
+
+1. Nc3'
+
+
 
 #### Part B ####
-echo "Pushing combined PGN..."
+echo "Pushing combined PGN of 2 games..."
 
 http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< '
 [White "White 1"]
@@ -31,25 +40,37 @@ http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROAD
 
 1. e4 e5
 
+
+[White "White 3"]
+[Black "Black 3"]
+[Result "*"]
+
+1. Nc3 Nf6'
+
+
+
+
+
+#### Part C ####
+echo "Pushing combined PGN of all 3 games..."
+
+http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< '
+[White "White 1"]
+[Black "Black 1"]
+[Result "*"]
+
+1. e4 e5 2. h4
+
+
 [White "White 2"]
 [Black "Black 2"]
 [Result "*"]
 
-1. e4 e5'
+1. d4 d5 2. a4
 
-#### Part C ####
-echo "Pushing each game individually..."
 
-http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< \
-'[White "White 1"]
-[Black "Black 1"]
+[White "White 3"]
+[Black "Black 3"]
 [Result "*"]
 
-1. e4 e5 2. Nf3'
-
-http -A bearer -a $API_TOKEN --body post $LICHESS_URL/api/broadcast/round/$BROADCAST_ROUND_ID/push <<< \
-'[White "White 2"]
-[Black "Black 2"]
-[Result "*"]
-
-1. e4 e5 2. Nf3'
+1. Nc3 Nf6 2. Nf3'
