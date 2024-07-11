0e2a0610119120ed28354a1af4bb525b364e76fb
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-07-11 07:55:05 -0400

    ts tweaks

--- src/upload.ts
+++ src/upload.ts
@@ -1,17 +1,17 @@
-import { recursiveFileList, add_to_queue, isSingleGamePgn } from './utils';
+import { recursiveFileList, add_to_queue, isSingleGamePgn, isMultiGamePgn } from './utils';
 
 export async function upload_gamespgn_file_if_exists(roundId: string, path: string) {
   const files = await recursiveFileList(path);
-  const gamespgn = files.find(file => file.endsWith('games.pgn'));
-
-  if (gamespgn) {
-    await add_to_queue(roundId, [gamespgn]);
-  }
+  await add_to_queue(
+    roundId,
+    files.filter(file => isMultiGamePgn(file)),
+  );
 }
 
 export async function uploadExistingFilesInFolder(roundId: string, path: string) {
-  let files = await recursiveFileList(path);
-  files = files.filter(file => isSingleGamePgn(file));
-
-  await add_to_queue(roundId, files);
+  const files = await recursiveFileList(path);
+  await add_to_queue(
+    roundId,
+    files.filter(file => isSingleGamePgn(file)),
+  );
 }
