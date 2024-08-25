35275de6625ef0a99efab663403acbace88933ff
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-08-25 19:50:46 -0400

    cleanup updater script

--- scripts/updater.py
+++ scripts/updater.py
@@ -1,25 +1,21 @@
-import datetime
 import json
 import os
 import requests
 
-with open(os.path.join(os.path.dirname(__file__), '../package.json')) as f:
-    app = json.load(f)
+release = requests.get('https://api.github.com/repos/lichess-org/broadcaster/releases/latest').json()
 
 signatures = {}
 signatures['mac']     = requests.get('https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-macos.app.tar.gz.sig').text
 signatures['linux']   = requests.get('https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-linux.AppImage.tar.gz.sig').text
 signatures['windows'] = requests.get('https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-windows.msi.zip.sig').text
 
-released_at = requests.get('https://api.github.com/repos/lichess-org/broadcaster/releases/latest').json()['published_at']
-
 version_file = os.path.join(os.path.dirname(__file__), '../updater/version.json')
 
 with open(version_file) as f:
     updater = json.load(f)
 
-updater['version'] = 'v' + app['version']
-updater['pub_date'] = released_at
+updater['version'] = release['tag_name']
+updater['pub_date'] = release['published_at']
 
 updater['platforms']['darwin-x86_64']['signature']  = signatures['mac']
 updater['platforms']['darwin-aarch64']['signature'] = signatures['mac']
