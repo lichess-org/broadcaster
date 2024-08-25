e388c72a9dca1a499688e1ad4163fdd606c0363f
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-08-24 21:22:15 -0400

    Automate the auot-update endpoint (#36)
    * Automate the auot-update endpoint
    
    * update
    
    * windows: use signature of msi.zip

--- README.md
+++ README.md
@@ -100,3 +100,10 @@ Release artifacts are in `src-tauri/target/release/bundle/`
    - Approve the signing request in the SignPath dashboard when the workflow gets to that step.
 
 3. When ready to recommend the update, update the ["Check for Updates" endpoint](https://lichess-org.github.io/broadcaster/version.json) ([source](https://github.com/lichess-org/broadcaster/blob/main/updater/version.json))
+
+   ```bash
+   python scripts/updater.py
+
+   # then push the changes to github
+   # github pages will automatically publish
+   ```
---
+++ scripts/updater.py
@@ -0,0 +1,31 @@
+import datetime
+import json
+import os
+import requests
+
+with open('package.json') as f:
+    app = json.load(f)
+
+signatures = {}
+signatures['mac']     = requests.get('https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-macos.app.tar.gz.sig').text
+signatures['linux']   = requests.get('https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-linux.AppImage.tar.gz.sig').text
+signatures['windows'] = requests.get('https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-windows.msi.zip.sig').text
+
+current_datetime = datetime.datetime.now().isoformat()
+
+version_file = os.path.join(os.path.dirname(__file__), '../updater/version.json')
+
+with open(version_file) as f:
+    version = json.load(f)
+
+version['version'] = 'v' + app['version']
+version['pub_date'] = current_datetime
+
+version['platforms']['darwin-x86_64']['signature']  = signatures['mac']
+version['platforms']['darwin-aarch64']['signature'] = signatures['mac']
+version['platforms']['linux-x86_64']['signature']   = signatures['linux']
+version['platforms']['windows-x86_64']['signature'] = signatures['windows']
+
+with open(version_file, 'w') as f:
+    json.dump(version, f, indent=2)
+    f.write('\n')
