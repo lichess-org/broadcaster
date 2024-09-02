b912e05fcdbb1819aefc74323c96f59e367dd9bf
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-02 16:55:48 +0200

    remove commented code
    Was part of the removed showBoard code

--- src/components/Round.vue
+++ src/components/Round.vue
@@ -171,32 +171,10 @@ getRound();
       </div>
     </div>
 
-      <iframe
-        :src="'https://lichess.org/embed/broadcast/_/_/' + round.round.id"
-        style="width: 100%; aspect-ratio: 4/3"
-        frameborder="0"
-      ></iframe>
-      <!-- <div class="float-right">
-        <SwitchGroup as="div" class="flex items-center">
-          <Switch
-            v-model="showBoard"
-            :class="[
-              showBoard ? 'bg-indigo-600' : 'bg-gray-200',
-              'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2',
-            ]"
-          >
-            <span
-              aria-hidden="true"
-              :class="[
-                showBoard ? 'translate-x-5' : 'translate-x-0',
-                'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
-              ]"
-            />
-          </Switch>
-          <SwitchLabel as="span" class="ml-3 text-sm">
-            <span class="font-medium text-gray-400">Show boards</span>
-          </SwitchLabel>
-        </SwitchGroup>
-      </div> -->
+    <iframe
+      :src="'https://lichess.org/embed/broadcast/_/_/' + round.round.id"
+      style="width: 100%; aspect-ratio: 4/3"
+      frameborder="0"
+    ></iframe>
   </template>
 </template>
