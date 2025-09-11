fe9d5f0db89f9130d93f753700aae5d412231d16
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-09-10 21:44:48 -0400

    updater script to work with new sig filenames

--- scripts/updater.py
+++ scripts/updater.py
@@ -6,9 +6,13 @@ import requests
 release = requests.get("https://api.github.com/repos/lichess-org/broadcaster/releases/latest").json()
 
 signatures = {}
-signatures["mac"] = requests.get("https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-macos.app.tar.gz.sig").text
-signatures["linux"] = requests.get("https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-linux.AppImage.tar.gz.sig").text
-signatures["windows"] = requests.get("https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-windows.msi.zip.sig").text
+signatures["mac"] = requests.get("https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-macos.app.tar.gz.sig")
+signatures["linux"] = requests.get("https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-linux.AppImage.sig")
+signatures["windows"] = requests.get("https://github.com/lichess-org/broadcaster/releases/latest/download/Lichess-Broadcaster-windows.msi.zip.sig")
+
+for platform, response in signatures.items():
+    if response.status_code != 200:
+        raise Exception(f"Error fetching signature for {platform}: {response.status_code}")
 
 version_file = os.path.join(os.path.dirname(__file__), "../updater/version.json")
 
@@ -18,10 +22,10 @@ with open(version_file) as f:
 updater["version"] = release["tag_name"]
 updater["pub_date"] = release["published_at"]
 
-updater["platforms"]["darwin-x86_64"]["signature"] = signatures["mac"]
-updater["platforms"]["darwin-aarch64"]["signature"] = signatures["mac"]
-updater["platforms"]["linux-x86_64"]["signature"] = signatures["linux"]
-updater["platforms"]["windows-x86_64"]["signature"] = signatures["windows"]
+updater["platforms"]["darwin-x86_64"]["signature"] = signatures["mac"].text
+updater["platforms"]["darwin-aarch64"]["signature"] = signatures["mac"].text
+updater["platforms"]["linux-x86_64"]["signature"] = signatures["linux"].text
+updater["platforms"]["windows-x86_64"]["signature"] = signatures["windows"].text
 
 with open(version_file, "w") as f:
     json.dump(updater, f, indent=2)
