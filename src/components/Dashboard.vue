ec9605b2cc7eab43da56fa2efbef7996aa38ee9e
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-12-01 21:47:27 -0500

    remove queuSize and game count from dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,7 +6,7 @@ const status = useStatusStore();
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-px bg-white/5">
+  <div class="grid grid-cols-2 gap-px bg-white/5">
     <div
       class="bg-gray-700 px-4 py-6 sm:px-6 lg:px-8"
       :class="{
@@ -19,23 +19,9 @@ const status = useStatusStore();
       </p>
     </div>
     <div class="bg-gray-700 px-4 py-6 sm:px-6 lg:px-8">
-      <p class="text-sm font-medium leading-6 text-gray-400">Upload Queue</p>
-      <p class="mt-2 flex items-baseline gap-x-2">
-        <!-- <span class="text-4xl font-semibold tracking-tight text-white">{{ logs.queueSize }}</span> -->
-        todo
-      </p>
-    </div>
-    <div class="bg-gray-700 px-4 py-6 sm:px-6 lg:px-8">
       <p class="text-sm font-medium leading-6 text-gray-400">Rounds</p>
       <p class="mt-2 flex items-baseline gap-x-2">
         <span class="text-4xl font-semibold tracking-tight text-white">{{ status.activeCount }}</span>
-      </p>
-    </div>
-    <div class="bg-gray-700 px-4 py-6 sm:px-6 lg:px-8">
-      <p class="text-sm font-medium leading-6 text-gray-400">Games</p>
-      <p class="mt-2 flex items-baseline gap-x-2">
-        <!-- <span class="text-4xl font-semibold tracking-tight text-white">{{ logs.files.size }}</span> -->
-        todo
       </p>
     </div>
   </div>
